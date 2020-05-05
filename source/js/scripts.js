9b63d0d76ee1ae6d913788976c33093f7e332b25
Vadim Rahmanov <VadimRahmanov@gmail.com>
2020-05-05 16:47:57 +0100

    Доработка 4

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,9 +1,6 @@
-var navigation = document.querySelector(".main-nav");
 var navListOne = document.querySelector(".main-nav__list--catalog-and-order");
 var navListTwo = document.querySelector(".main-nav__list--search-and-basket");
 var menuButton = document.querySelector(".header__toggle");
-var openMenu = document.querySelector(".header__toggle-button");
-var closeMenu = document.querySelector(".header__toggle-close");
 
 menuButton.classList.remove("header__toggle--hidden");
 navListOne.classList.add("main-nav-hidden");
@@ -12,6 +9,5 @@ navListTwo.classList.add("main-nav-hidden");
 menuButton.addEventListener("click", function () {
   navListOne.classList.toggle("main-nav-hidden"),
   navListTwo.classList.toggle("main-nav-hidden"),
-  openMenu.classList.toggle("header__toggle--hidden"),
-  closeMenu.classList.toggle("header__toggle--hidden");
+  menuButton.classList.toggle("header__toggle--close");
 });
